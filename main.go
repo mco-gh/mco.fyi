4d424256592a4c5dcb0a92ac684619b4ca87ab88
marcacohen <marcacohen@gmail.com>
2020-02-19 17:12:18 +0000

    update

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ import (
 func redirect(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimLeft(r.URL.Path, "/")
 	fmt.Println(path)
-	if url, ok := links[path]; ok {
+	if path == "" {
+		http.ServeFile(w, r, "home.html")
+	} else if path == "meiko.jpg" {
+		http.ServeFile(w, r, "meiko.jpg")
+	} else if url, ok := links[path]; ok {
 		//do something here
 		http.Redirect(w, r, url.(string), 301)
 	} else {
